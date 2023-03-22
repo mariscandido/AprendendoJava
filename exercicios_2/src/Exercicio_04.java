b951541722bac67d774e5d9c12a6a301fe097ff5
Mariana Silveira <mariscandido3107@gmail.com>
2023-03-22 15:54:34 -0300

    Enviando versao 1

---
+++ exercicios_2/src/Exercicio_04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio_04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int id, hrt;
+		double vl, salario;
+		
+		id = sc.nextInt();
+		hrt = sc.nextInt();
+		vl = sc.nextDouble();
+		
+		salario = hrt * vl;
+		
+		System.out.println("NUMBER = " + id);
+		System.out.printf("SALARY = U$ %.2f", salario);
+		
+
+		sc.close();
+	}
+
+}
