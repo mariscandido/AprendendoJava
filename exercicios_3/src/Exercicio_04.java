3276fa480e6bbb5fc3a6caa0238dfcbc50531cf5
Mariana Silveira <mariscandido3107@gmail.com>
2023-03-22 21:14:32 -0300

    Atualizacao dos exercicios

--- exercicios_3/src/Exercicio_04.java
+++ exercicios_3/src/Exercicio_04.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class Exercicio_04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Digite a hora inicial do jogo ");
+		int hi = sc.nextInt();
+		System.out.println("Digite a hora final do jogo ");
+		int hf = sc.nextInt();
+
+		int duracao;
+
+		if (hi < hf) {
+			duracao = hf - hi;
+		} else {
+			duracao = 24 - hi + hf;
+		}
+
+		System.out.println("O jogo durou: " + duracao + "hora(s)");
+
+		sc.close();
 	}
 
 }
